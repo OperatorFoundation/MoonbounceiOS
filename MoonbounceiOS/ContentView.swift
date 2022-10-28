72faad3e71f89214623a542e3758834787b32984
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2022-10-28 17:09:01 -0500

    Updated Home Screen UI

--- MoonbounceiOS/ContentView.swift
+++ MoonbounceiOS/ContentView.swift
@@ -10,22 +10,52 @@ import SwiftUI
 struct ContentView: View {
     @State private var connectButtonPressed = false
     @State private var buttonTitle = ""
+    @State private var ipAddress: String = ""
+    @State private var port: String = ""
+    @State private var disallow: String = ""
+    @State private var exclude: String = ""
+    
+    let configInfoLabel = "Enter Transport Information:"
     
     var body: some View {
-        NavigationView {
-                Button("Connect!") {
+        VStack(alignment: .center, spacing: 15)
+        {
+            Text("Moonbounce")
+                .bold()
+                .font(.largeTitle).foregroundColor(Color.black)
+                //.padding([.top], 40) //.bottom]
+            GroupBox(configInfoLabel)
+            {
+                TextField("Server IP Address", text: $ipAddress) //Text("IP Address: \(ipAddress)")
+                TextField("Server Port Number", text: $port) //Text("Port: \(port)")
+                TextField("The App to Disallow", text: $disallow)
+                TextField("The Route to Exclude", text: $exclude)
+            }
+            .padding(40)
+                Button("Connect to VPN!")
+                {
                     connectPressed()
                 }
-                    .accessibilityLabel("Connect")
-                    .buttonStyle(.borderedProminent)
-                    .tint(.indigo)
-        .navigationTitle("MoonbounceiOS")
-        .navigationBarTitleDisplayMode(.inline)
-        }
-        .alert(buttonTitle, isPresented: $connectButtonPressed) {
-            Button("Ok", role: .cancel) {}
-        } message: {
-            Text("Connect Button Pressed!")
+                .accessibilityLabel("Connect")
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+                .navigationBarTitleDisplayMode(.inline)
+                Button("Test TCP")
+                {
+                    // do something
+                }
+                .accessibilityLabel("Connect")
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+                .navigationBarTitleDisplayMode(.inline)
+                Button("Test UDP")
+                {
+                    // do something
+                }
+                .accessibilityLabel("Connect")
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
     
